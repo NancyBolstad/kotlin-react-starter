f1c8f59eb34cd89280a19974335630dcfcb25940
NancyBolstad <litchacha@gmail.com>
2020-06-14 17:11:25 +0200

    Initial commit test

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+import react.dom.*
+import kotlin.browser.document
+
+fun main(){
+    render(document.getElementById("root")){
+        head {                                                     // 2
+            title { +"HTML encoding with Kotlin" }
+        }
+        h1 {
+            +"KotlinConf Explorer"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            p {
+                +"John Doe: Building and breaking things"
+            }
+            p {
+                +"Jane Smith: The development process"
+            }
+            p {
+                +"Matt Miller: The Web 7.0"
+            }
+
+            h3 {
+                +"Videos watched"
+            }
+            p {
+                +"Tom Jerry: Mouseless development"
+            }
+        }
+        div {
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+    }
+}
